a57973f76adb1c640ab3c21d65d93552a2dcc2ee
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-03-27 20:41:58 +0300

    Updated indentation

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,11 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that goes through binary tree using in-order
- *@tree: pointer to the root node of the tree to traverse.
- *@func: pointer to a function to call for each node.
+ * binary_tree_postorder - goes through a binary tree using post-order.
+ *@tree:pointer to the root node of the tree to traverse.
+ *@func:pointer to a function to call for each node.
  */
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
