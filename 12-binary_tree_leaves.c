5ff4c914fd62680ff662d1749ec6adc06c6481e5
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-03-27 23:37:24 +0300

    Updated coding styling

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,20 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf -  a function that checks if a node is a leaf.
- *@node: is a pointer to the node to check.
- * Return: return 1 if node is a leaf, otherwise 0.
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: Pointer to the root node of the tree to count the leaves
+ *
+ * Return: Number of leaves in the binary tree, or 0 if tree is NULL
  */
-
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (1);
-	}
-	return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+    if (tree == NULL)
+        return (0);
+
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+
+    return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 }
