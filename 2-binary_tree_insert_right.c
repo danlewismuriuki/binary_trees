708a54324af3fb559aa58274a35c281b870df093
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-03-27 03:07:01 +0300

    Added spacing aroumd operator

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,15 +11,16 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL;
 
-	if (parent==NULL)
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
 	node = malloc(sizeof(binary_tree_t));
-	if (node==NULL)
+	if (node == NULL)
 	{
 		return (NULL);
 	}
+
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
